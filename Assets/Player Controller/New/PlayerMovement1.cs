3d6858efc63061e769903d2583acc1d563c00c67
LCADNW <noahwade@lcad.edu>
2026-02-28 16:14:42 -0800

    Tried Movement Controller

--- Assets/Player Controller/New/PlayerMovement1.cs
+++ Assets/Player Controller/New/PlayerMovement1.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement1 : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float playerSpeed = 5f;
